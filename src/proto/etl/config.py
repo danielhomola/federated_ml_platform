33acd15bdd858799cf0112e436347d88320b63c4
Daniel Homola <daniel@neoglia.com>
2019-07-29 15:31:41 +0100

    making sure proto works on my home pc too

--- src/proto/etl/config.py
+++ src/proto/etl/config.py
@@ -8,7 +8,7 @@ class SSHInfoEicu(object):
     Info to connect to our AWS EC2 with eICU DB.
     """
     host = ("ec2-3-9-28-102.eu-west-2.compute.amazonaws.com", 22)
-    ssh_private_key = "/home/daniel/.ssh/aws_uk_laptop.pem"
+    ssh_private_key = "~/.ssh/aws_uk_laptop.pem"
     ssh_username = "ubuntu"
     remote_bind_address = ("localhost", 5432)
 
